6f9f94c52ae2a19639ab7cd58940feb61519ce17
Md. Tazwarul Islam Abir <md.tazwarul.islam.07@gmail.com>
2024-01-20 20:02:27 +0600

    try to impliment socket.io 5

--- index.js
+++ index.js
@@ -19,17 +19,18 @@ const io = new Server(httpServer,{
 
 
 io.on('connection', (socket) => {
+
+
   socket.on("user",(email)=>{
     console.log(email+'connected')
 
-    
+    socket.on('allTask-from-client',(data=>{
+      socket.emit('all-task-from-server',data)
+    }))
+
   })
 
-  socket.emit('fromServer','server-msg')
-
-  socket.on('allTask-from-client',(data=>{
-    socket.emit('all-task-from-server',data)
-  }))
+ 
 
 })
 
