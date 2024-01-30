c77f22ffce205c26b3a49c8cb106827f6b5ee6e5
Md. Tazwarul Islam Abir <md.tazwarul.islam.07@gmail.com>
2024-01-31 00:45:40 +0600

    move socket to other server

--- index.js
+++ index.js
@@ -1,80 +1,80 @@
 const express = require("express");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
+//const { createServer } = require("http");
+//const { Server } = require("socket.io");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
 
-const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: '*',
-  methods: ["GET", "POST"],
-});
+//const httpServer = createServer(app);
+//const io = new Server(httpServer, {
+ // cors: '*',
+ // methods: ["GET", "POST"],
+//});
 
 var jwt = require("jsonwebtoken");
 
-let onlineUsers = [];
+//let onlineUsers = [];
 
-io.on("connection", (socket) => {
+// io.on("connection", (socket) => {
 
-  socket.on("users", (email) => {
-    //console.log('A user connected ',socket.id,email)
-    const isExist = onlineUsers.find((user) => user.email == email);
-    if (!isExist && email) {
-      onlineUsers.push({ email: email, socketID: socket.id });
-      io.emit("connectedUsers", onlineUsers);
-    }
-  });
+//   socket.on("users", (email) => {
+//     //console.log('A user connected ',socket.id,email)
+//     const isExist = onlineUsers.find((user) => user.email == email);
+//     if (!isExist && email) {
+//       onlineUsers.push({ email: email, socketID: socket.id });
+//       io.emit("connectedUsers", onlineUsers);
+//     }
+//   });
 
-  //recive msg from sender and send to reciver person
-  socket.on("msgFromSender", (data) => {
-    io.to(data.receverSocketID.socketID).emit("reciver", {
-      connectionID: data.connectionID,
-      msgData: data.msgData,
-    });
-    io.to(data.senderSocketID.socketID).emit("reciver", {
-      connectionID: data.connectionID,
-      msgData: data.msgData,
-    });
-  });
+//   //recive msg from sender and send to reciver person
+//   socket.on("msgFromSender", (data) => {
+//     io.to(data.receverSocketID.socketID).emit("reciver", {
+//       connectionID: data.connectionID,
+//       msgData: data.msgData,
+//     });
+//     io.to(data.senderSocketID.socketID).emit("reciver", {
+//       connectionID: data.connectionID,
+//       msgData: data.msgData,
+//     });
+//   });
 
-  //recive friend req
-  socket.on('pendingReq',(data)=>{
-    console.log(data)
-    const isExist = onlineUsers.find((user) => user.email == data.reciver.email)
-    if(isExist){
-      io.to(isExist.socketID).emit('pendingReqs',data.msg)
-    }
-  })
-// accept connection status 
+//   //recive friend req
+//   socket.on('pendingReq',(data)=>{
+//     console.log(data)
+//     const isExist = onlineUsers.find((user) => user.email == data.reciver.email)
+//     if(isExist){
+//       io.to(isExist.socketID).emit('pendingReqs',data.msg)
+//     }
+//   })
+// // accept connection status 
 
-socket.on('connectionStatus',(data=>{
-  const isExist = onlineUsers.find((user) => user.email == data.email)
-  if(isExist){
-    io.to(isExist.socketID).emit('connection_Status',data.msg)
-  }
-}))
+// socket.on('connectionStatus',(data=>{
+//   const isExist = onlineUsers.find((user) => user.email == data.email)
+//   if(isExist){
+//     io.to(isExist.socketID).emit('connection_Status',data.msg)
+//   }
+// }))
 
-//real time add task
-socket.on('taskAdded',(data=>{
-  io.emit('newTaskAdded',data)
-}))
+// //real time add task
+// socket.on('taskAdded',(data=>{
+//   io.emit('newTaskAdded',data)
+// }))
 
-//real time task delete
-socket.on('taskDeleted',(data=>{
-  io.emit('deletedTask',data)
-}))
+// //real time task delete
+// socket.on('taskDeleted',(data=>{
+//   io.emit('deletedTask',data)
+// }))
 
-  socket.on("disconnect", () => {
+//   socket.on("disconnect", () => {
 
-    console.log('user disconnected',socket.id);
+//     console.log('user disconnected',socket.id);
 
-    onlineUsers = onlineUsers.filter((user) => user.socketID !== socket.id);
-    io.emit("connectedUsers", onlineUsers);
-    //console.log(onlineUsers,'after dc',)
-  });
+//     onlineUsers = onlineUsers.filter((user) => user.socketID !== socket.id);
+//     io.emit("connectedUsers", onlineUsers);
+//     //console.log(onlineUsers,'after dc',)
+//   });
 
-});
+// });
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
@@ -396,7 +396,10 @@ app.get("/getAllUserChat/:email", verifyJWT, async (req, res) => {
 const port = process.env.PORT || 3000;
 
 
-httpServer.listen(port, () => {
+// httpServer.listen(port, () => {
+//   console.log(`task-managemnet app listening on port ${port}`);
+// });
+
+app.listen(port, () => {
   console.log(`task-managemnet app listening on port ${port}`);
 });
-
